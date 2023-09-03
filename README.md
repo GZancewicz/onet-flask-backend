242e99de3c77a4289a172667a5eb4a9306a6032f
Greg Zancewicz <greg.zancewicz@gmail.com>
2023-09-03 07:05:01 -0500

    heartbeat

---
+++ README.md
@@ -0,0 +1,4 @@
+## Prerequisites
+
+1. `pip install Flask`
+2. `pip install FastAPI`
---
+++ api.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/heartbeat", methods=["GET"])
+def heartbeat():
+    return jsonify({"status": "ok"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
