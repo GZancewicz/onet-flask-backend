432845ce6465c2ff1446d6808aeb9044c17eb674
Greg Zancewicz <greg.zancewicz@gmail.com>
2023-10-08 12:15:58 -0500

    Add playlist data

--- util/onet-v2-utils/fetch_youtube.py
+++ util/onet-v2-utils/fetch_youtube.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import requests
 import json
-import os
+import requests
 from youtube_credentials import get_api_key
 
 # Constants
@@ -9,13 +8,83 @@ from youtube_credentials import get_api_key
 CHANNEL_ID = "UCb_MxJmD_J4peQtRHj-qBKA"  # Replace with the channel ID of @orthodoxnet
 SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 VIDEOS_URL = "https://www.googleapis.com/youtube/v3/videos"
-MAX_RESULTS = 100
+PLAYLIST_URL = "https://www.googleapis.com/youtube/v3/playlistItems"
+MAX_RESULTS = 20
+HOMILIES_PLAYLIST = "PLEh2brXYUf7kJYXxrBKAxXSKYdl46IHde"
+CHILDRENS_PLAYLIST = "PLEh2brXYUf7nIigGqPthPFw4ApKiIu9zW"
+CATECHISM_PLAYLIST = "PLEh2brXYUf7k06I4K0dgAJC11A3TlgpFK"
 
 
-def fetch_last_videos(api_key, channel_id, max_results):
-    # Fetch video IDs first
+def fetch_homilies_playlist(max_results=MAX_RESULTS):
+    return fetch_latest_playlist_videos(HOMILIES_PLAYLIST, max_results)
+
+
+def fetch_childrens_playlist(max_results=MAX_RESULTS):
+    return fetch_latest_playlist_videos(CHILDRENS_PLAYLIST, max_results)
+
+
+def fetch_catechism_playlist(max_results=MAX_RESULTS):
+    return fetch_latest_playlist_videos(CATECHISM_PLAYLIST, max_results)
+
+
+def fetch_video_data(video_ids):
     params = {
         "key": get_api_key(),
+        "id": ",".join(video_ids),
+        "part": "snippet,statistics,contentDetails",
+    }
+    response = requests.get(VIDEOS_URL, params=params)
+    response_data = response.json()
+
+    return response_data
+
+
+def fetch_playlist_video_ids(playlist_id, max_results):
+    params = {
+        "key": get_api_key(),
+        "playlistId": playlist_id,
+        "order": "date",
+        "part": "snippet",
+        "maxResults": max_results,
+    }
+    response = requests.get(PLAYLIST_URL, params=params)
+    response_data = response.json()
+
+    video_ids = [
+        item["snippet"]["resourceId"]["videoId"] for item in response_data["items"]
+    ]
+    return video_ids
+
+
+def fetch_latest_playlist_videos(playlist_id, max_results=MAX_RESULTS):
+    video_ids = fetch_playlist_video_ids(playlist_id, max_results)
+    response_data = fetch_video_data(video_ids)
+
+    videos = []
+
+    for item in response_data["items"]:
+        video_data = {
+            "title": item["snippet"]["title"],
+            "description": item["snippet"]["description"],
+            "url": f"https://www.youtube.com/watch?v={item['id']}",
+            "thumbnail": item["snippet"]["thumbnails"]["high"]["url"],
+            "published_at": item["snippet"]["publishedAt"],
+            "channel_title": item["snippet"]["channelTitle"],
+            "duration": item["contentDetails"]["duration"],
+            "view_count": item["statistics"]["viewCount"],
+            "like_count": item["statistics"].get("likeCount", "N/A"),
+            "dislike_count": item["statistics"].get("dislikeCount", "N/A"),
+        }
+        videos.append(video_data)
+
+    return videos
+
+
+def fetch_latest_video_ids(
+    api_key=get_api_key(), channel_id=CHANNEL_ID, max_results=MAX_RESULTS
+):
+    params = {
+        "key": api_key,
         "channelId": channel_id,
         "order": "date",
         "part": "snippet",
@@ -34,14 +103,45 @@ def fetch_last_videos(api_key, channel_id, max_results):
 
     video_ids = [item["id"]["videoId"] for item in response_data["items"]]
 
-    # Fetch video details
-    params = {
-        "key": api_key,
-        "id": ",".join(video_ids),
-        "part": "snippet,statistics,contentDetails",
-    }
-    response = requests.get(VIDEOS_URL, params=params)
-    response_data = response.json()
+    return video_ids
+
+
+def fetch_latest_videos(
+    api_key=get_api_key(), channel_id=CHANNEL_ID, max_results=MAX_RESULTS
+):
+    video_ids = fetch_latest_video_ids(api_key, channel_id, max_results)
+
+    # # Fetch video IDs first
+    # params = {
+    #     "key": api_key,
+    #     "channelId": channel_id,
+    #     "order": "date",
+    #     "part": "snippet",
+    #     "type": "video",
+    #     "maxResults": max_results,
+    # }
+    # response = requests.get(SEARCH_URL, params=params)
+    # response_data = response.json()
+
+    # if "items" not in response_data:
+    #     print(
+    #         "Error fetching videos:",
+    #         response_data.get("error", {}).get("message", "Unknown error"),
+    #     )
+    #     return []
+
+    # video_ids = [item["id"]["videoId"] for item in response_data["items"]]
+
+    # # Fetch video details
+    # params = {
+    #     "key": api_key,
+    #     "id": ",".join(video_ids),
+    #     "part": "snippet,statistics,contentDetails",
+    # }
+    # response = requests.get(VIDEOS_URL, params=params)
+    # response_data = response.json()
+
+    response_data = fetch_video_data(video_ids)
 
     videos = []
 
@@ -64,33 +164,6 @@ def fetch_last_videos(api_key, channel_id, max_results):
 
 
 if __name__ == "__main__":
-    # Fetch new videos
-    new_videos = fetch_last_videos(get_api_key(), CHANNEL_ID, MAX_RESULTS)
-    print(f"Fetched {len(new_videos)} new videos.")
-
-    # For debugging: print the first new video title
-    if new_videos:
-        print(f"First new video title: {new_videos[0]['title']}")
-
-    # Check if videos.json exists
-    filepath = "./content/videos.json"
-    if os.path.exists(filepath):
-        # Read existing videos from videos.json
-        with open(filepath, "r", encoding="utf-8") as f:
-            existing_videos = json.load(f)
-        print(f"Found {len(existing_videos)} existing videos.")
-
-        # For debugging: print the first existing video title
-        if existing_videos:
-            print(f"First existing video title: {existing_videos[0]['title']}")
-
-        # Prepend new videos to existing videos
-        new_videos = new_videos + existing_videos  # Prepending new videos
-    else:
-        print("No existing videos found.")
-
-    # Save the updated videos list back to videos.json
-    with open(filepath, "w", encoding="utf-8") as f:
-        json.dump(new_videos, f, ensure_ascii=False, indent=4)
-
-    print(f"{len(new_videos)} videos saved to {filepath}.")
+    latest_videos = fetch_catechism_playlist()
+    # latest_videos = fetch_latest_videos()
+    print(json.dumps(latest_videos, indent=2))
