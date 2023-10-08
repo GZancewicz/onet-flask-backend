80b7ebf1ac170840aa7a5fc619937ff3c0b0627b
Greg Zancewicz <greg.zancewicz@gmail.com>
2023-10-08 12:51:37 -0500

    Add playlist support

--- app.py
+++ app.py
@@ -3,19 +3,12 @@ from flask_cors import CORS
 import os
 import json
 
-# from fetch_youtube import (
-#     fetch_latest_videos,
-#     fetch_childrens_playlist,
-#     fetch_catechism_playlist,
-#     fetch_homilies_playlist,
-# )
-# from fetch_youtube import (
-#     fetch_latest_videos,
-#     fetch_childrens_playlist,
-#     fetch_catechism_playlist,
-#     fetch_homilies_playlist,
-# )
-from fetch_youtube import fetch_latest_videos
+from fetch_youtube import (
+    fetch_latest_videos,
+    fetch_childrens_playlist,
+    fetch_catechism_playlist,
+    fetch_homilies_playlist,
+)
 from fetch_calendar import return_calendar_events
 
 
@@ -46,31 +39,31 @@ def get_latest_videos():
         return jsonify({"error": "No videos found"}), 404
 
 
-# @app.route("/childrens_videos", methods=["GET"])
-# def get_childrens_videos():
-#     latest_videos = fetch_childrens_playlist()
-#     if latest_videos:
-#         return jsonify(latest_videos), 200
-#     else:
-#         return jsonify({"error": "No videos found"}), 404
+@app.route("/childrens_videos", methods=["GET"])
+def get_childrens_videos():
+    latest_videos = fetch_childrens_playlist()
+    if latest_videos:
+        return jsonify(latest_videos), 200
+    else:
+        return jsonify({"error": "No videos found"}), 404
 
 
-# @app.route("/catechism_videos", methods=["GET"])
-# def get_catechism_videos():
-#     latest_videos = fetch_catechism_playlist()
-#     if latest_videos:
-#         return jsonify(latest_videos), 200
-#     else:
-#         return jsonify({"error": "No videos found"}), 404
+@app.route("/catechism_videos", methods=["GET"])
+def get_catechism_videos():
+    latest_videos = fetch_catechism_playlist()
+    if latest_videos:
+        return jsonify(latest_videos), 200
+    else:
+        return jsonify({"error": "No videos found"}), 404
 
 
-# @app.route("/homilies_videos", methods=["GET"])
-# def get_homilies_videos():
-#     latest_videos = fetch_homilies_playlist()
-#     if latest_videos:
-#         return jsonify(latest_videos), 200
-#     else:
-#         return jsonify({"error": "No videos found"}), 404
+@app.route("/homilies_videos", methods=["GET"])
+def get_homilies_videos():
+    latest_videos = fetch_homilies_playlist()
+    if latest_videos:
+        return jsonify(latest_videos), 200
+    else:
+        return jsonify({"error": "No videos found"}), 404
 
 
 if __name__ == "__main__":
