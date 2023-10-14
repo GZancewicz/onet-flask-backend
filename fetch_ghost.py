9c15fb3398108f0a866b589a7f37fa7c3986b72b
Greg Zancewicz <greg.zancewicz@gmail.com>
2023-10-14 09:17:38 -0500

    Interface to ghost.io content API

---
+++ fetch_ghost.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+from flask import Flask, jsonify
+from flask_cors import CORS
+import json
+import requests
+
+# See https://ghost.org/docs/content-api
+
+BASE_URL = "https://orthodox-net.ghost.io/ghost/api/content/"
+POSTS_ENDPOINT = "/posts/"
+TAGS_ENDPOINT = "/tags/"
+GHOST_API_KEY = "fc614c68ef24717d70c30606ba"
+
+
+def posts_url():
+    return BASE_URL + POSTS_ENDPOINT
+
+
+def tags_url():
+    return BASE_URL + TAGS_ENDPOINT
+
+
+def post_url(post_id):
+    return BASE_URL + POSTS_ENDPOINT + post_id + "/"
+
+
+# https://orthodox-net.ghost.io/ghost/api/content/posts/?key=fc614c68ef24717d70c30606ba
+def fetch_posts():
+    params = {"key": GHOST_API_KEY, "include": "tags,authors"}
+    response = requests.get(posts_url(), params=params)
+    response_data = response.json()
+
+    return response_data
+
+
+def fetch_tags():
+    params = {"key": GHOST_API_KEY}
+    response = requests.get(tags_url(), params=params)
+    response_data = response.json()
+
+    return response_data
+
+
+def fetch_tagged_posts(slug):
+    params = {"key": GHOST_API_KEY, "filter": "tag:" + slug, "include": "tags,authors"}
+    response = requests.get(posts_url(), params=params)
+    response_data = response.json()
+
+    return response_data
+
+
+def fetch_post(post_id):
+    params = {"key": GHOST_API_KEY, "include": "tags,authors"}
+    response = requests.get(post_url(post_id), params=params)
+    response_data = response.json()
+
+    return response_data
+
+
+# if __name__ == "__main__":
+    # print(json.dumps(fetch_posts(), indent=2))
+    # print(json.dumps(fetch_tags(), indent=2))
+    # print(json.dumps(fetch_tagged_posts("orthodox_net"), indent=2))
+    print(json÷.dumps(fetch_post("652a8f969a71080001718f5b"), indent=2))
