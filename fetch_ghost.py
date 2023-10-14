ef0a1d07f21df4967292b6b5ea300d4cf8aaa91e
Greg Zancewicz <greg.zancewicz@gmail.com>
2023-10-14 09:18:49 -0500

    Comment out test code

--- fetch_ghost.py
+++ fetch_ghost.py
@@ -58,7 +58,7 @@ def fetch_post(post_id):
 
 
 # if __name__ == "__main__":
-    # print(json.dumps(fetch_posts(), indent=2))
-    # print(json.dumps(fetch_tags(), indent=2))
-    # print(json.dumps(fetch_tagged_posts("orthodox_net"), indent=2))
-    print(json÷.dumps(fetch_post("652a8f969a71080001718f5b"), indent=2))
+# print(json.dumps(fetch_posts(), indent=2))
+# print(json.dumps(fetch_tags(), indent=2))
+# print(json.dumps(fetch_tagged_posts("orthodox_net"), indent=2))
+# print(json÷.dumps(fetch_post("652a8f969a71080001718f5b"), indent=2))
