870e8abba6e76f17247adf691f8e82df84a62548
Greg Zancewicz <greg.zancewicz@gmail.com>
2024-07-07 08:56:05 -0500

    Fetch article metadata

--- fetch_ghost.py
+++ fetch_ghost.py
@@ -50,6 +50,14 @@ def fetch_tagged_posts(slug):
     return response_data
 
 
+def fetch_tagged_post_data(slug):
+    params = {"key": GHOST_API_KEY, "filter": "tag:" + slug, "include": "tags,authors"}
+    response = requests.get(posts_url(), params=params)
+    response_data = response.json()
+
+    return fetch_ids_from_tagged_post_data(response_data)
+
+
 def fetch_post(post_id):
     params = {"key": GHOST_API_KEY, "include": "tags,authors"}
     response = requests.get(post_url(post_id), params=params)
@@ -89,9 +97,33 @@ def fetch_article(post_id):
     }
 
 
+def fetch_ids_from_tagged_post_data(response_data):
+    return [
+        {
+            "id": post["id"],
+            "title": post["title"],
+            "excerpt": post["custom_excerpt"],
+            "posted_at": post["created_at"],
+            "tags": fetch_all_tags_from_tagged_post(post["tags"]),
+            "authors": fetch_all_authors_from_tagged_post(post["authors"]),
+        }
+        for post in response_data["posts"]
+    ]
+
+
+def fetch_all_tags_from_tagged_post(data):
+    return [item["name"] for item in data]
+
+
+def fetch_all_authors_from_tagged_post(data):
+    return [item["name"] for item in data]
+
+
 if __name__ == "__main__":
     # print(json.dumps(fetch_posts(), indent=2))
     # print(json.dumps(fetch_tags(), indent=2))
-    print(json.dumps(fetch_tagged_posts("orthodox_net"), indent=2))
+    # print(json.dumps(fetch_tagged_posts("orthodox_net"), indent=2))
+    data = fetch_tagged_posts("orthodox_net")
+    print(json.dumps(fetch_ids_from_tagged_post_data(data), indent=2))
     # print(json.dumps(fetch_post("652a8f969a71080001718f5b"), indent=2))
     # print(json.dumps(fetch_article("652a8f969a71080001718f5b"), indent=2))
